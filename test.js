39fdc7f94ec3c6d2a4a29cea01ff8dbf7fedd3f3
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-11-24 01:39:18 +0000

    Prueba.

--- test.js
+++ test.js
@@ -7,16 +7,16 @@ test('adds 14 + 9 to equal 23', () => {
     expect(total).toBe(23);
 });
 
-test("One euro should be 1.206 dollars", function()
+test ("One euro should be 1.206 dollars", function()
 {
     
     const { fromEuroToDollar } = require('./app.js')
     
-    // hago mi comparacion (la prueba)
+   
     expect(fromEuroToDollar(3.5)).toBe(4.2); 
 })
 
-test("One dollar should be 153.48 yen", function()
+test ("One dollar should be 153.48 yen", function()
 {
     
     const { fromDollarToYen } = require('./app.js')
@@ -25,7 +25,7 @@ test("One dollar should be 153.48 yen", function()
     expect(fromDollarToYen(1)).toBe(153.48); 
 })
 
-test("One yen should be 0.0062 pound", function()
+test ("One yen should be 0.0062 pound", function()
 {
     
     const { fromYenToPound } = require('./app.js')
